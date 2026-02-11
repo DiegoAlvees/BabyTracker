865f749d80528674061079a43c90424bf47313d6
Diego Alves <ediegoalves@gmail.com>
2026-02-11 14:18:20 -0300

    add metodo GET  para acordar API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { API_URL } from './config/api.config';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,20 @@ export class App {
 
   showApiWarning = signal(true);
 
+constructor(private http: HttpClient) {}
+
   ngOnInit() {
     setTimeout(() => {
       this.showApiWarning.set(false)
     }, 9000);
+
+    this.wakeApi()
+  }
+
+  private wakeApi() {
+    this.http.get(`${API_URL}/babies?userId=1`).subscribe({
+      next: () => console.log('API acordou!'),
+      error: () => console.log('API ainda demorando...')
+    })
   }
 }
